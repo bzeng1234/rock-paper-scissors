f8dd86dbbfb33c4719b6c9858f3a2eabe9cc0d1c
Brandon Zeng <bzeng1234@gmail.com>
2022-08-22 21:36:25 -0700

    gui initial commit

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,42 +1,59 @@
 let user_score = 0;
 let comp_score = 0;
+let round_count = 0;
+
+let gameResult = document.createElement('div');
+gameResult.setAttribute('style', 'font-family: Verdana, sans-serif; text-align: center; padding-top: 20px;')
+
+let bodySelector = document.querySelector('body');
+
+const userChoice = document.querySelectorAll("button");
+userChoice.forEach(choice => choice.addEventListener('click', game));
 
 function getComputerChoice() {
-    let choice_arr = ["Rock", "Paper", "Scissors"];
+    let choice_arr = ["Rock", "Paper", "Scissor"];
     return choice_arr[Math.floor(Math.random()*3)].toLowerCase();
-}
-
-function playerSelection() {
-    let user_choice = prompt("Select Rock, Paper, or Scissors");
-    return user_choice.toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection) {
     // tie scenario
-    if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")) {
-        return `Tie Game. Both players choose ${playerSelection}`;
-    } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+    if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissor" && computerSelection === "scissor")) {
+        return `Tie Game. Both players choose ${playerSelection} ${user_score} - ${comp_score}`;
+    } else if ((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "scissor" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
         user_score++;
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        return `You Win! ${playerSelection} beats ${computerSelection} ${user_score} - ${comp_score}`;
     } else {
         comp_score++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        return `You Lose! ${computerSelection} beats ${playerSelection} ${user_score} - ${comp_score}`;
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let comp_choice = getComputerChoice();
-        let user_choice = playerSelection();
-        console.log(playRound(user_choice, comp_choice));
-    }
+function game(e) {
+    let user_choice = e.target.value.toLowerCase();
+    let comp_choice = getComputerChoice();
 
-    if (user_score > comp_score) {
-        console.log("Player Wins");
-    } else if (comp_score > user_score) {
-        console.log("Computer Wins");
-    } else {
-        console.log("Tie Game");
-    }
+    gameResult.textContent =  playRound(user_choice, comp_choice);
     
+    bodySelector.appendChild(gameResult);
+    round_count++;
+
+    if (round_count == 5) {
+        if (user_score > comp_score) {
+            gameResult.textContent = `Player Wins ${user_score} - ${comp_score}`;
+            bodySelector.appendChild(gameResult);
+        } else if (comp_score > user_score) {
+            gameResult.textContent = `Computer Wins ${user_score} - ${comp_score}`;
+            bodySelector.appendChild(gameResult);
+        } else {
+            gameResult.textContent = `Tie Game ${user_score} - ${comp_score}`;
+            bodySelector.appendChild(gameResult);
+        }
+        resetGame();
+    } 
+}
+
+function resetGame() {
+    round_count = 0;
+    user_score = 0;
+    comp_score = 0;
 }
